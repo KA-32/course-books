ba51d8020edf70857c6a7a498332321fc3347f5e
Bhimashankar Sutar <bhima@piktorlabs.com>
2020-06-13 17:45:29 +0530

    added searchbox component.

---
+++ src/components/SearchBox/SearchBox.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+import AutoSuggestion from "../AutoSuggestion/AutoSuggestion";
+
+import "./SearchBox.css";
+
+const SearchBox = (props) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [isSearchQuerySelected, setSearchQuerySelctionState] = useState(false);
+  const [isSuggestionsVisible, setSuggestionsVisibility] = useState(true);
+
+  const handleSubmit = (e) => {
+    props.addBook(searchQuery);
+    setSearchQuery("");
+    setSearchQuerySelctionState(false);
+  };
+
+  const handleTextChange = (value) => {
+    let filterTitles = props.titles.filter((title, index) => {
+      return title.indexOf(value) > -1;
+    });
+    setSearchQuery(value);
+    setSuggestionsVisibility(true);
+    setSuggestions(filterTitles);
+    setSearchQuerySelctionState(false);
+  };
+
+  const onSelected = (value) => {
+    setSearchQuery(value);
+    setSuggestionsVisibility(false);
+    setSearchQuerySelctionState(true);
+  };
+
+  return (
+    <div className="form-search">
+      <input
+        type="text"
+        aria-required="true"
+        aria-label="Search box"
+        name="book-search"
+        placeholder="Enter book name"
+        className="search-input"
+        value={searchQuery}
+        onChange={(e) => {
+          handleTextChange(e.target.value);
+        }}
+      />
+      {searchQuery && isSuggestionsVisible && (
+        <AutoSuggestion onSelected={onSelected} data={suggestions} />
+      )}
+      <button
+        onClick={(e) => {
+          handleSubmit(e);
+        }}
+        disabled={isSearchQuerySelected ? false : true}
+        className="form-submit-btn"
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default SearchBox;

511e1fc1e35d5d307add7572ce9d348dbf9c1e0d
Bhimashankar Sutar <bhima@piktorlabs.com>
2020-06-13 20:25:16 +0530

    base test cases added.

---
+++ src/components/CourseBook/CourseBook.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import CourseBook from "./CourseBook";
+
+describe("Course Book", () => {
+  test("Should match empty card.", () => {
+    const courseBook = render(<CourseBook title="" author="" description="" />);
+    expect(courseBook).toMatchSnapshot();
+  });
+
+  test("Should render props correctly", () => {
+    const { getByText } = render(
+      <CourseBook
+        title="New Title"
+        author="Test Author"
+        description="Some description"
+      />
+    );
+    const titleElement = getByText(/New Title/i);
+    const authorElement = getByText(/Test Author/i);
+    const descriptionElement = getByText(/Some description/i);
+
+    expect(titleElement).toBeInTheDocument();
+    expect(authorElement).toBeInTheDocument();
+    expect(descriptionElement).toBeInTheDocument();
+  });
+});

23e32563a031b5a02b4a391c8e5386c85d80199e
Bhimashankar Sutar <bhima@piktorlabs.com>
2020-06-13 19:08:37 +0530

    added test case to match snapshot.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { render } from "@testing-library/react";
+
 import App from "./App";
 
-test("Title Should be 'Search Books'", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Search Books/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  test("Title Should be 'Search Books'", () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/Search Books/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("App to match snapshot", () => {
+    const app = render(<App />);
+    expect(app).toMatchSnapshot();
+  });
 });

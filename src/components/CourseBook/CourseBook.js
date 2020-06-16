b709d0d9420b63183e33fde797e3ccffbeed71ee
Bhimashankar Sutar <bhima@piktorlabs.com>
2020-06-16 18:15:28 +0530

    added read more/less functionality

--- src/components/CourseBook/CourseBook.js
+++ src/components/CourseBook/CourseBook.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 import avatarIcon from "../../assets/ic-avatar.png";
@@ -10,20 +10,40 @@ import "./CourseBook.css";
  * Show selected Course Book.
  */
 const CourseBook = ({ index, title, author, description }) => {
-  let listItemClassName = (index> 0 && (index+1) % 3 === 0) ? "book-item":"book-item margin"
+  const [isReadMore, setReadMoreStatus] = useState(false);
 
+  useEffect(() => {}, []);
+  const handleReadMoreClick = (e) => {
+    setReadMoreStatus(!isReadMore);
+  };
+
+  let listItemClassName =
+    index > 0 && (index + 1) % 3 === 0 ? "book-item" : "book-item margin";
 
   return (
     <Fragment>
       <li className={listItemClassName}>
         <h2 className="title">{title}</h2>
-        <p className="description">{description}</p>
+        <div className="description-wrapper">
+          <p className="description">
+            {description.length > 300
+              ? isReadMore
+                ? description
+                : description.substring(0, 300) + "..."
+              : description}
+          </p>
+          {description.length > 300 && (
+            <button className="read-more" onClick={handleReadMoreClick}>
+              {isReadMore ? "Read Less" : "Read More"}
+            </button>
+          )}
+        </div>
         <div className="avatar-wrapper">
           <img src={avatarIcon} alt="Author" className="author-image" />
-          <span className="author-name">{author}</span>
+          <p className="author-name">{author}</p>
         </div>
       </li>
-      {index> 0 && (index+1) % 3 === 0 && <li className="break"></li>}
+      {index > 0 && (index + 1) % 3 === 0 && <li className="break"></li>}
     </Fragment>
   );
 };

c631caf70a4e99369a07627f9c57cb61ca53b6a6
Bhimashankar Sutar <bhima@piktorlabs.com>
2020-06-16 14:21:33 +0530

    code cleanup

--- src/utils/coursebooks.test.js
+++ src/utils/coursebooks.test.js
@@ -17,8 +17,16 @@ describe("CourseBooks utility class",()=>{
         expect(CourseBooks.getSummaries().length).toBe(55);
     });
 
+    test("Should match first summary",()=>{
+        // expect(CourseBooks.getSummaries()).toContain('The Book in Three Sentences: Practicing meditation and mindfulness will make you at least 10 percent happier. Being mindful doesn\u2019t change the problems in your life, but mindfulness does help you respond to your problems rather than react to them. Mindfulness helps you realize that striving for success is fine as long as you accept that the outcome is outside your control.');
+    });
+
     test("total 55 authors should be present",()=>{
         expect(CourseBooks.getAuthors().length).toBe(55);
+    });
+
+    test("Should have author",()=>{
+        // expect(CourseBooks.getAuthors()).toContain('Dan Harris');
     });
 
 });
